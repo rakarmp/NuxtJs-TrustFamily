df97dd19d605a8761548a201eec83d960cbe7304
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-10-29 23:57:48 +0700

    Finish From Localhost

---
+++ pages/index.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="container">
+    <CBox
+      v-bind="mainStyles[colorMode]"
+      d="flex"
+      w="100vw"
+      h="100vh"
+      flex-dir="column"
+      justify-content="center"
+    >
+      <CHeading text-align="center" mb="4">
+        ⚡️ Hello, Family Trust <br>Learning,Focus,On Direct
+      </CHeading>
+      <CFlex justify="center" direction="column" align="center">
+        <CBox mb="3">
+          <CIconButton
+            mr="3"
+            :icon="colorMode === 'light' ? 'moon' : 'sun'"
+            :aria-label="`Switch to ${
+              colorMode === 'light' ? 'dark' : 'light'
+            } mode`"
+            @click="toggleColorMode"
+          />
+          <CButton
+            left-icon=""
+            variant-color="blue"
+            @click="showToast"
+          >
+            Klik Dong
+          </CButton>
+        </CBox>
+        <CAvatarGroup>
+          <CAvatar
+            name="Evan You"
+            alt="Evan You"
+            src="https://res.cloudinary.com/dnshfgo9i/image/upload/v1667062460/WhatsApp_Image_2022-10-29_at_23.46.19_qhe5yg.jpg"
+          >
+            <CAvatarBadge size="1.0em" bg="green.500" />
+          </CAvatar>
+          <CAvatar
+            name="Jonathan Bakebwa"
+            alt="Jonathan Bakebwa"
+            src="https://res.cloudinary.com/dnshfgo9i/image/upload/v1667062465/WhatsApp_Image_2022-10-29_at_23.47.09_npbk2z.jpg"
+          >
+            <CAvatarBadge size="1.0em" bg="green.500" />
+          </CAvatar>
+          <!-- <CAvatar
+            name="Segun Adebayo"
+            alt="Segun Adebayo"
+            src="https://pbs.twimg.com/profile_images/1169353373012897802/skPUWd6e_400x400.jpg"
+          >
+            <CAvatarBadge size="1.0em" bg="green.500" />
+          </CAvatar> -->
+          <!-- <CAvatar src="pop">
+            <CAvatarBadge size="1.0em" border-color="papayawhip" bg="tomato" />
+          </CAvatar>  -->
+        </CAvatarGroup>
+        <!-- <CButton
+          left-icon="close"
+          variant-color="red"
+          mt="3"
+          @click="showModal = true"
+        >
+          Delete Account
+        </CButton> -->
+        <!-- <CModal :is-open="showModal">
+          <CModalOverlay />
+          <CModalContent>
+            <CModalHeader>Are you sure?</CModalHeader>
+            <CModalBody>Deleting user cannot be undone</CModalBody>
+            <CModalFooter>
+              <CButton @click="showModal = false">
+                Cancel
+              </CButton>
+              <CButton
+                margin-left="3"
+                variant-color="red"
+                @click="showModal = false"
+              >
+                Delete User
+              </CButton>
+              
+            </CModalFooter>
+            <CModalCloseButton @click="showModal = false" />
+          </CModalContent>
+        </CModal> -->
+      </CFlex>
+    </CBox>
+  </div>
+</template>
+
+<script lang="js">
+import {
+  CBox,
+  CButton,
+  CAvatarGroup,
+  CAvatar,
+  CAvatarBadge,
+  CModal,
+  CModalContent,
+  CModalOverlay,
+  CModalHeader,
+  CModalFooter,
+  CModalBody,
+  CModalCloseButton,
+  CIconButton,
+  CFlex,
+  CHeading
+} from '@chakra-ui/vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    CBox,
+    CButton,
+    CAvatarGroup,
+    CAvatar,
+    CAvatarBadge,
+    CModal,
+    CModalContent,
+    CModalOverlay,
+    CModalHeader,
+    CModalFooter,
+    CModalBody,
+    CModalCloseButton,
+    CIconButton,
+    CFlex,
+    CHeading
+  },
+  inject: ['$chakraColorMode', '$toggleColorMode'],
+  data () {
+    return {
+      showModal: false,
+      mainStyles: {
+        dark: {
+          bg: 'gray.700',
+          color: 'whiteAlpha.900'
+        },
+        light: {
+          bg: 'white',
+          color: 'gray.900'
+        }
+      }
+    }
+  },
+  computed: {
+    colorMode () {
+      return this.$chakraColorMode()
+    },
+    theme () {
+      return this.$chakraTheme()
+    },
+    toggleColorMode () {
+      return this.$toggleColorMode
+    }
+  },
+  methods: {
+    showToast () {
+      this.$toast({
+        title: 'Makasih Lohh.',
+        description: "Respect For You.",
+        status: 'success',
+        duration: 10000,
+        isClosable: true
+      })
+    }
+  }
+}
+</script>
